0b24c2d6d1333109f42d2e1ad1d20752a4b74c4a
giuseppesciacca <sciaccagi@gmail.com>
2023-04-13 16:47:20 +0200

    [add] header +npm sass & bootstrap +img

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,87 @@ export default {
 </script>
 
 <template>
-  <div>
+  <header id="app_header" class="p-3">
+    <div class="container">
+      <div class="row">
 
-  </div>
+        <div class="logo col-5">
+          <img class="img-fluid" src="./assets/img/dc-logo.png" alt="">
+        </div>
+
+        <nav class="navbar navbar-expand col-7">
+          <ul class="nav navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">CHARACTERS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">COMICS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">MOVIES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">TV</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">GAMES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">COLLECTIBLES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">VIDEOS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">FANS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">NEWS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">SHOP</a>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+    </div>
+  </header>
+  <!-- /#app_header -->
+
+
+
+  <main id="app_main" class="">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, dolore! Cum magni reprehenderit minima temporibus
+    facilis accusamus amet illo, perferendis esse voluptates tempora, nulla illum nihil iste debitis commodi fugit?
+  </main>
+  <!-- /#app_main -->
+  <footer id="app_footer">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ex quod ut est vel quis ducimus, eveniet, iure dolore
+    excepturi in ipsam possimus rem sed eum nemo. Debitis, eaque culpa!
+  </footer>
+  <!-- /#app_footer -->
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#app_header {
+  height: 120px;
+
+  .logo img {
+    height: 80px;
+  }
+
+  a {
+    color: black;
+  }
+}
+
+#app_main {
+  background-color: red;
+}
+
+#app_footer {
+  background-color: blue;
+}
+</style>
