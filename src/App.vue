5bf9ec5f95300b8dd4ed081b5599d40fdb7dcdf9
giuseppesciacca <sciaccagi@gmail.com>
2023-04-14 10:40:30 +0200

    [add] AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
 <script>
-import linkMenu from './assets/data/linkMenu';
+import AppHeader from './components/AppHeader.vue'
 
 export default {
-  data() {
-    return {
-      linkMenu: linkMenu
-    }
-  }
+  name: 'App',
+  components: {
+    AppHeader,
+  },
 }
 </script>
 
 <template>
-  <header id="app_header" class="p-3">
-    <div class="container">
-      <div class="row">
-
-        <div class="logo col-5">
-          <img class="img-fluid" src="./assets/img/dc-logo.png" alt="">
-        </div>
-
-        <nav class="navbar navbar-expand col-7">
-          <ul class="nav navbar-nav">
-            <li v-for="link in linkMenu" class="nav-item">
-              <a class="nav-link" :href="link.src">{{ link.text }}</a>
-            </li>
-          </ul>
-        </nav>
-
-      </div>
-    </div>
-  </header>
+  <AppHeader></AppHeader>
   <!-- /#app_header -->
-
-
 
   <main id="app_main" class=" debug">
 
@@ -223,23 +202,6 @@ export default {
 
 
 <style lang="scss" scoped>
-#app_header {
-  height: 120px;
-
-  .logo img {
-    height: 80px;
-  }
-
-  a {
-    color: rgba(70, 70, 73, 1);
-    font-weight: 600;
-  }
-
-  li:hover a {
-    color: rgba(2, 130, 249, 1);
-  }
-}
-
 main.debug {
   background-color: rgba(28, 28, 28, 1);
   color: white;
