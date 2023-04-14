30943aad8f357a4087a068811ced5e6e56077338
giuseppesciacca <sciaccagi@gmail.com>
2023-04-14 10:26:29 +0200

    [add] file scss, delete file css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/scss/app.scss'
 import App from './App.vue'
-import bootstrap from '../node_modules/bootstrap/scss/bootstrap.scss'
 
 createApp(App).mount('#app')
